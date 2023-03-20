43b93ef6a48e599d79b049ae32b343e8e45889b8
1596944197 <1596944197@qq.com>
2023-03-20 10:59:33 +0800

    feat:增加错误捕获

--- index.mjs
+++ index.mjs
@@ -20,15 +20,15 @@ const bot = new TelegramBot(token, { polling: true });
 
 // Matches "/echo [whatever]"
 bot.onText(/\/b (.+)/, async (msg, match) => {
-  // 'msg' is the received Message from Telegram
-  // 'match' is the result of executing the regexp above on the text content
-  // of the message
 
   const chatId = msg.chat.id;
-  const resp = match?.[1] || ''; // the captured "whatever"
+  const resp = match?.[1] || '';
 
-  const aiText = await gpt.sendMessage(resp)
-
-  // send back the matched "whatever" to the chat
-  bot.sendMessage(chatId, aiText);
+  try {
+    const aiText = await gpt.sendMessage(resp)
+    bot.sendMessage(chatId, aiText);
+  } catch (error) {
+    console.warn(error)
+    bot.sendMessage(chatId, '出错了');
+  }
 });
